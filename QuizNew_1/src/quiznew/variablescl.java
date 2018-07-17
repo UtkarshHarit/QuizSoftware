219ed44bf537fd05f19758412959290b393109b2
UtkarshHarit <35928153+UtkarshHarit@users.noreply.github.com>
2018-07-18 04:21:41 +0530

    Add files via upload

---
+++ QuizNew_1/src/quiznew/variablescl.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiznew;
+
+/**
+ *
+ * @author utkar
+ */
+public class variablescl {
+Q1 q=new Q1();
+    
+    public int sum=q.q1+q.q10+q.q2+q.q3+q.q4+q.q5+q.q6+q.q7+q.q8+q.q9;
+    
+}
